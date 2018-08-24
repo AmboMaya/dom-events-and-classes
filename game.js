84541e1c8d946c4a71a088ff6c713d1acf36e7b2
Ice Maya <echaymaya@gmail.com>
2018-08-24 21:37:05 +1200

    completed game.js challenge

--- game.js
+++ game.js
@@ -15,7 +15,6 @@ function bindEventListeners (dots) {
     dots[i].addEventListener('click', makeBlue)
     dots[i].addEventListener('dblclick', hide)
 
-
   }
 }
 
@@ -45,9 +44,18 @@ function updateCounts () {
   }
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
-
+  var dots = document.getElementsByClassName('board')[0].children
+  for (var i = 0; i < dots.length; i++) {
+    if (dots[i].classList.contains('blue')) {
+      totals.blue++; 
+    } else if (dots[i].classList.contains('green')) {
+      totals.green++;
+    } else {
+      totals.invisible++
+    }
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
+}
 }
 
 function displayTotals (totals) {
@@ -55,3 +63,4 @@ function displayTotals (totals) {
     document.getElementById(key + '-total').innerHTML = totals[key]
   }
 }
+
